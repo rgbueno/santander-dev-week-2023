c3136a2504e1136abf44decfd5ae97bf8c736f10
Rodrigo Gomes Bueno <rodrigobueno.ti@gmail.com>
2024-09-12 19:37:49 -0300

    Correção precision e scale

--- src/main/java/me/dio/domain/model/Card.java
+++ src/main/java/me/dio/domain/model/Card.java
@@ -17,7 +17,7 @@ public class Card {
 	@Column(unique = true)
 	private String number;
 	
-	@Column(name = "avaible_limit", scale = 13, precision = 2)
+	@Column(name = "avaible_limit", precision = 13, scale = 2)
 	private BigDecimal limit;
 	
 	public Long getId() {
